d66f81bab82d97b93297af8c4ac1a9e0368f93bd
Сорокин Тимофей <timabananaman@gmail.com>
2023-12-03 12:32:09 +0300

    Авторизация + обновление токена каждые 4 минуты.

--- src/App.js
+++ src/App.js
@@ -3,19 +3,29 @@ import './App.css';
 import HomePage from './pages/HomePage';
 import LoginPage from "./pages/LoginPage";
 
+import {AuthProvider} from "./context/AuthContext";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from "./components/Header";
+import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
     return (
         <div className="App">
+
             <Router>
-                <Header/>
-                <Routes>
-                    <Route element={<HomePage/>} path="/"/>
-                    <Route element={<LoginPage/>} path="/login"/>
-                </Routes>
+                <AuthProvider>
+                    <Header/>
+                    <Routes>
+                        <Route path="/" element={
+                            <PrivateRoute>
+                                <HomePage/>
+                            </PrivateRoute>
+                        }/>
+                        <Route element={<LoginPage/>} path="/login"/>
+                    </Routes>
+                </AuthProvider>
             </Router>
+
         </div>
     );
 }
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useContext} from "react";
+import AuthContext from "../context/AuthContext";
 
 const HomePage = () => {
+    let {loginUser} = useContext(AuthContext);
     return (
         <div>
-            <form>
+            <form onSubmit={loginUser}>
                 <input type="text" name="username" placeholder="Enter username"/>
                 <input type="password" name="password" placeholder="Enter password"/>
                 <button type="submit">Login</button>
