8a1a20fb5d4d205284a9831f1f2a706c686fe3db
Сорокин Тимофей <timabananaman@gmail.com>
2023-12-04 23:31:53 +0300

    fix

--- src/components/PollsPage/PollsForm.js
+++ src/components/PollsPage/PollsForm.js
@@ -1,46 +1,47 @@
 import React, {useContext, useEffect, useState} from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import AuthContext from "../../context/AuthContext";
 
 const Polls = () => {
-  const [polls, setPolls] = useState([]);
-  const navigate = useNavigate();
-  let {authTokens} = useContext(AuthContext);
+    const [polls, setPolls] = useState([]);
+    const navigate = useNavigate();
+    let {authTokens} = useContext(AuthContext);
 
-  const fetchPolls = async () => {
-    try {
-      const response = await fetch('http://127.0.0.1:8000/api/polls', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: 'Bearer ' + String(authTokens.access),
-        },
-      });
-      const data = await response.json();
-      if (response.status === 200) {
-        setPolls(data);
-      } else {
-        alert('Something went wrong!');
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    const fetchPolls = async () => {
+        try {
+            const response = await fetch('http://127.0.0.1:8000/api/polls/', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + String(authTokens.access),
+                },
+            });
+            const data = await response.json();
+            if (response.status === 200) {
+                console.log(data)
+                setPolls(data);
+            } else {
+                alert('Something went wrong!');
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-  useEffect(() => {
-    fetchPolls();
-  }, []);
+    useEffect(() => {
+        fetchPolls();
+    }, []);
 
-  return (
-    <div>
-      <h3>Список опросов:</h3>
-      {polls.map((poll) => (
-        <div key={poll.poll_id}>
-          <Link to={`/polls/${poll.poll_id}`}>{poll.name_of_a_poll}</Link>
+    return (
+        <div>
+            <h3>Список опросов:</h3>
+            {polls.map((poll) => (
+                <div key={poll.poll_id}>
+                    <Link to={`/polls/${poll.poll_id}`}>{poll.name_of_a_poll}</Link>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default Polls;
