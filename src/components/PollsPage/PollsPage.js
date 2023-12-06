4da46be631550b5c13aa2fcd1221bfdd440d0fa2
Сорокин Тимофей <timabananaman@gmail.com>
2023-12-06 09:12:58 +0300

    Some refactor changes

--- src/components/PollsPage/PollsPage.js
+++ src/components/PollsPage/PollsPage.js
@@ -1,13 +1,48 @@
-import React from "react";
-import PollsForm from './PollsForm';
+import React, {useContext, useEffect, useState} from "react";
 
 import './PollsPage.css';
+import {Link, useNavigate} from "react-router-dom";
+import AuthContext from "../../context/AuthContext";
 
 const PollsPage = () => {
+    const [polls, setPolls] = useState([]);
+    const navigate = useNavigate();
+    let {authTokens} = useContext(AuthContext);
+
+    const fetchPolls = async () => {
+        try {
+            const response = await fetch('http://127.0.0.1:8000/api/polls/', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + String(authTokens.access),
+                },
+            });
+            const data = await response.json();
+            if (response.status === 200) {
+                console.log(data)
+                setPolls(data);
+            } else {
+                alert('Something went wrong!');
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchPolls();
+    }, []);
+
     return (
-      <div className="PollsPage">
-        <PollsForm/>
-      </div>
+        <div>
+            <h3>Список опросов:</h3>
+            {polls.map((poll) => (
+                <div key={poll.poll_id}>
+                    <Link to={`/polls/${poll.poll_id}`}>{poll.name_of_a_poll}</Link>
+                </div>
+            ))}
+        </div>
     );
 
 }
