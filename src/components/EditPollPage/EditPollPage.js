2559c3ac36bd203a93b37869e5a0c222c8270796
zallixx <almoroz08@gmail.com>
2023-12-12 20:24:23 +0300

    EditPollPage.js - начало

---
+++ src/components/EditPollPage/EditPollPage.js
@@ -0,0 +1,11 @@
+import React, {useContext, useEffect, useState} from "react";
+
+import './EditPollPage.css';
+import AuthContext from "../../context/AuthContext";
+
+const EditPollPage = () => {
+    return (
+    );
+}
+
+export default EditPollPage;
