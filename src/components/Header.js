ccce1b5e4e5b280b9e5f2d641b637bf5e2bd2c3b
zallixx <almoroz08@gmail.com>
2023-12-14 13:48:27 +0300

    навбар

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,16 @@
-import cx from 'clsx';
 import { useState } from 'react';
 import {
   Container,
-  Group,
   Text,
-  Menu,
   Tabs,
   MantineProvider,
 } from '@mantine/core';
-import classes from './Header.module.css';
 import {useNavigate} from "react-router-dom";
+import {ButtonGroup} from "react-bootstrap";
 
 const user = {
-  name: 'Jane Spoonfighter',
-  email: 'janspoon@fighter.dev',
+  name: '',
+  email: '',
   image: '',
 };
 
@@ -32,7 +29,6 @@ const menu_items = {
 };
 
 const Header = () => {
-    const [userMenuOpened, setUserMenuOpened] = useState(false);
     const navigate = useNavigate()
     const [isOpen, setIsOpen] = useState(false);
 
@@ -46,48 +42,59 @@ const Header = () => {
 
     const handleMenuItemClick = (item) => {
         const path = menu_items[item];
-        console.log(item);
         if (path) {
             navigate(path);
         }
     };
 
     const items = Object.keys(tabs).map((tab) => (
-        <Tabs.Tab value={tab} key={tab} onClick={() => handleTabClick(tab)}>
+        <button className="btn border-0 rounded-0" style={{backgroundColor: 'slategray', color: 'black', }} value={tab} key={tab} onClick={() => handleTabClick(tab)}>
             {tab}
-        </Tabs.Tab>
+        </button>
       ));
 
     return (
         <MantineProvider>
-            <div className={classes.dropdown}>
-                <button className={classes.button} onClick={handleButtonClick} style={{ position: 'absolute', right: 0 }}>
-                    <Text>{user.name}</Text>
-                </button>
-                {isOpen && (
-                    <div className={classes.menu} style={{ position: 'absolute', right: 0, top: '5%' }}>
-                        <ul className={classes.menuItems}>
-                            <button className={cx(classes.menuItem)} onClick={() => handleMenuItemClick('Liked polls')}>
+            <button className="btn border-0 rounded-0" onClick={handleButtonClick}
+                    style={{position: 'absolute', right: 0, backgroundColor: 'slategray', color: 'black', height: '3.2%'}}
+            >
+                {user.name}
+            </button>
+            {isOpen && (
+                <div style={{position: 'absolute', right: 0, top: '4%'}}>
+                    <ButtonGroup>
+                        <ul>
+                            <button className="btn btn-info mb-2"
+                                    onClick={() => handleMenuItemClick('Liked polls')}>
                                 Liked polls
                             </button>
                             <br/>
-                            <button className={cx(classes.menuItem)} onClick={() => handleMenuItemClick('Complains')}>
+                            <button className="btn btn-info mb-2" onClick={() => handleMenuItemClick('Complains')}>
                                 Complains
                             </button>
                             <br/>
-                            <button className={cx(classes.menuItem)} onClick={() => handleMenuItemClick('Your comments')}>
+                            <button className="btn btn-info mb-2" onClick={() => handleMenuItemClick('Your comments')}>
                                 Your comments
                             </button>
                             <br/>
-                            <button className={cx(classes.menuItem)} onClick={() => handleMenuItemClick('Logout')}>
+                            <button className="btn btn-danger mb-2" onClick={() => handleMenuItemClick('Logout')}>
                                 Logout
                             </button>
                         </ul>
-                    </div>
-                )}
+                    </ButtonGroup>
+                </div>
+            )}
+            <div style={{position: 'absolute', marginTop: '0%', right: '40%', left: '40%'}}>
+                <Container size="md">
+                    <Tabs>
+                        <Tabs.List>
+                            {items}
+                        </Tabs.List>
+                    </Tabs>
+                </Container>
             </div>
         </MantineProvider>
-    );
+);
 };
 
 export default Header
