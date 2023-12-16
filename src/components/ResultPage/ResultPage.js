654f2dba11f6f251ec3584cd734d729449115e3b
Сорокин Тимофей <timabananaman@gmail.com>
2023-12-16 17:38:49 +0300

    Страница результатов, прогрессбар, галочка если проголосовал

--- src/components/ResultPage/ResultPage.js
+++ src/components/ResultPage/ResultPage.js
@@ -6,11 +6,12 @@ import ReactLoading from "react-loading";
 const ResultPage = () => {
     const [isLoading, setLoading] = useState(true);
     const [results, setResults] = useState([]);
-    let {authTokens} = useContext(AuthContext);
+    let {user, authTokens} = useContext(AuthContext);
     const params = useParams();
+    const [num_of_votes, setNumOfVotes] = useState(0);
 
     useEffect(() => {
-        fetchResults().then(() => setLoading(false));
+        fetchResults();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
     const fetchResults = async () => {
@@ -25,6 +26,12 @@ const ResultPage = () => {
             const data = await response.json();
             if (response.status === 200) {
                 setResults(data);
+                let num = 0
+                for (let i = 0; i < data.choices.length; i++) {
+                    num += data.choices[i][1];
+                }
+                setNumOfVotes(num);
+                setLoading(false);
             } else {
                 alert('Something went wrong!');
             }
@@ -48,10 +55,41 @@ const ResultPage = () => {
                         <div
                             className="result"
                             key={result.id}>
-                            <h2>{result[0]}</h2>
-                            <p>{result[1]} votes</p>
+                            <div className="result-item">
+                                <div className="result-name">
+                                    {result[0]}
+                                </div>
+                                <div className="result-percent">
+                                    {Math.round(result[1] / num_of_votes * 100)}%
+                                </div>
+                                {result[2].includes(user.user_id) ? (
+                                    <div className="result-checkmark">
+                                        <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-check"
+                                             fill="currentColor"
+                                             xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd"
+                                                  d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06l-4.5 4.5z"/>
+                                        </svg>
+                                    </div>
+                                ) : (
+                                    <></>
+                                )}
+                                <div
+                                    className="progress">
+                                    <div
+                                        className="progress-bar"
+                                        role="progressbar"
+                                        style={{width: Math.round(result[1] / num_of_votes * 100) + '%'}}
+                                        aria-valuenow={Math.round(result[1] / num_of_votes * 100)}
+                                        aria-valuemin="0"
+                                        aria-valuemax="100">
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+
                     ))}
+
                 </div>
             </div>
         </div>
