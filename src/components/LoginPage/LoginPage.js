0aae4985b30e118ec07f1dc8d8e396517ddbcde7
Сорокин Тимофей <timabananaman@gmail.com>
2023-12-16 15:48:32 +0300

    Фикс возможности повторно авторизоваться

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, {useContext} from "react";
 import LoginForm from './LoginForm';
+import AuthContext from "../../context/AuthContext";
+import {useNavigate} from "react-router-dom";
 
 const LoginPage = () => {
+    let {user} = useContext(AuthContext);
+    const navigate = useNavigate();
+    if (user) {
+        navigate('/');
+        return;
+    }
     return (
       <div className="BasePageCss">
+
         <LoginForm/>
       </div>
     );
--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useContext} from "react";
 import RegisterForm from './RegisterForm';
+import AuthContext from "../../context/AuthContext";
+import {useNavigate} from "react-router-dom";
 
 const RegisterPage = () => {
+    let {user} = useContext(AuthContext);
+    const navigate = useNavigate();
+    if (user) {
+        navigate('/');
+        return;
+    }
     return (
-      <div className="BasePageCss">
-        <RegisterForm/>
-      </div>
+        <div className="BasePageCss">
+            <RegisterForm/>
+        </div>
     );
 
 }
