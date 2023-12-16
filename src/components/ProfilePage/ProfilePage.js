bf1e87fd37089be52d11ace9cd4a3f3f520c7e27
zallixx <almoroz08@gmail.com>
2023-12-17 00:40:17 +0300

    modal dark style

--- src/components/ProfilePage/ProfilePage.js
+++ src/components/ProfilePage/ProfilePage.js
@@ -3,7 +3,6 @@ import AuthContext from "../../context/AuthContext";
 import { BsFeather } from "react-icons/bs";
 import ReactLoading from "react-loading";
 
-import { BsFillXSquareFill } from "react-icons/bs";
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
@@ -79,7 +78,7 @@ const ProfilePage = () => {
         <div className="BasePageCss">
             <div className="body-wrapper">
                 <div className="body-inner h-100 w-75">
-                    <h3>Настройки</h3>
+                    <h4 style={{ justifyContent: 'center', display: 'flex' }}>Настройки</h4>
                     <div style={{display: 'flex', flexDirection: 'row'}}>
                         <h5> Ваш логин: {userInfo.username} </h5>
                         <div style={{marginLeft: '10px', color: '#2980b9', cursor: 'pointer'}}>
@@ -110,24 +109,19 @@ const ProfilePage = () => {
                         </h5>
                     </div>
                 </div>
-                <Modal show={show} onHide={handleClose} centered bsPrefix="" className="rounded-1">
-                    <Modal.Header bsPrefix={"custom-modal-" + localStorage.getItem("theme")}>
-                        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-                            <Modal.Title>{title_of_modal}</Modal.Title>
-                            <div style={{color: '#2980b9', cursor: 'pointer', marginTop: '-4px'}}>
-                                <BsFillXSquareFill onClick={handleClose}/>
-                            </div>
-                        </div>
-                        <hr/>
+                <Modal show={show} onHide={handleClose} centered className={"custom-modal-" + localStorage.getItem("theme")}>
+                    <Modal.Header className="rounded-top-1 border-0">
+                        <Modal.Title>{title_of_modal}</Modal.Title>
                     </Modal.Header>
-                    <Modal.Body bsPrefix={"custom-modal-" + localStorage.getItem("theme")}>
+                    <Modal.Body>
+                        <input type="text" className="form-control" placeholder="Новое значение"/>
                     </Modal.Body>
-                    <Modal.Footer bsPrefix={"custom-modal-" + localStorage.getItem("theme")}>
-                        <Button variant="secondary" onClick={handleClose}>
-                            Close
-                        </Button>
+                    <Modal.Footer closeButton className="rounded-bottom-1 border-0">
                         <Button variant="primary" onClick={handleClose}>
-                            Save Changes
+                            Изменить
+                        </Button>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Отмена
                         </Button>
                     </Modal.Footer>
                 </Modal>
