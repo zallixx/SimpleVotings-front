ed60a6eec1d3b38884536f01bf5d2be2cb7245a6
Сорокин Тимофей <timabananaman@gmail.com>
2023-12-17 16:41:43 +0300

    Вывод текста, если пользователь не принимал участия в опросах

--- src/components/ProfilePage/ProfilePage.js
+++ src/components/ProfilePage/ProfilePage.js
@@ -85,7 +85,7 @@ const ComplainsPage = () => {
         fetchUser()
     }, []);
 
-    if (isLoading) {
+    if (isLoading && !user) {
         return (
             <ReactLoading className="position-fixed top-50 start-50 translate-middle h3" height={'6%'} width={'6%'}
                           type="bubbles" color="#505253"/>
@@ -97,59 +97,70 @@ const ComplainsPage = () => {
             <div className="body-wrapper">
                 <div className="body-inner h-100 w-75 position-relative">
                     {user.is_public === true ? (
-                        <div>
-                            <h4 style={{justifyContent: 'center', display: 'flex'}}>Вы просматриваете профиль
-                                пользователя
-                                - {user.username}</h4>
-                            <hr/>
                             <div>
-                                {user.avatar === null ? (
-                                    <label>Данный пользователь не установил аватар</label>
-                                ) : (
-                                    <img src={user.avatar} alt="avatar" width="200" height="200"/>
-                                )
-                                }
-                            </div>
-                            <hr/>
-                            <div>
-                                <p style={{justifyContent: 'center', display: 'flex'}}>
-                                    <b>Его цитата - {user.quote}</b>
-                                </p>
-                            </div>
-                            <hr/>
-                            <div>
-                                {user.show_history === true ? (
-                                        <div>
-                                            <h5> История голосования пользователя</h5>
-                                            <div
-                                                className="d-flex flex-column flex-md-row p-4 gap-4 py-md-5 align-items-center justify-content-center">
-                                                <div className="list-group list-group-checkable h-100 w-100 rounded">
-                                                    {Object.values(questions).map((question) => (
-                                                        <div
-                                                            className="list-group-item list-group-item-action weak_blue rounded">
-                                                            {/* eslint-disable-next-line  */}
-                                                            <a onClick={() => navigate(`/polls/${question.id}/results/`)}>
-                                                                <div className="d-flex w-100 justify-content-between">
-                                                                    <h5 className="mb-1">{'Принял участние в опросе: ' + question.question}</h5>
-                                                                    <small>Нажмите, чтобы посмотреть результат</small>
-                                                                </div>
-                                                            </a>
-                                                        </div>
-                                                    ))}
+                                <h4 style={{justifyContent: 'center', display: 'flex'}}>Вы просматриваете профиль
+                                    пользователя
+                                    - {user.username}</h4>
+                                <hr/>
+                                <div>
+                                    {user.avatar === null ? (
+                                        <label>Данный пользователь не установил аватар</label>
+                                    ) : (
+                                        <img src={user.avatar} alt="avatar" width="200" height="200"/>
+                                    )
+                                    }
+                                </div>
+                                <hr/>
+                                <div>
+                                    <p style={{justifyContent: 'center', display: 'flex'}}>
+                                        <b>Его цитата - {user.quote}</b>
+                                    </p>
+                                </div>
+                                <hr/>
+                                <div>
+                                    {user.show_history === true ? (
+                                            <div>
+                                                <h5> История голосования пользователя</h5>
+                                                <div
+                                                    className="d-flex flex-column flex-md-row p-4 gap-4 py-md-5 align-items-center justify-content-center">
+                                                    <div className="list-group list-group-checkable h-100 w-100 rounded">
+                                                        {Object.values(questions).length > 0 ? (
+
+                                                                Object.values(questions).map((question) => (
+                                                                    <div
+                                                                        className="list-group-item list-group-item-action weak_blue rounded">
+                                                                        {/* eslint-disable-next-line  */}
+                                                                        <a onClick={() => navigate(`/polls/${question.id}/results/`)}>
+                                                                            <div
+                                                                                className="d-flex w-100 justify-content-between">
+                                                                                <h5 className="mb-1">{'Принял участние в опросе: ' + question.question}</h5>
+                                                                                <small>Нажмите, чтобы посмотреть
+                                                                                    результат</small>
+                                                                            </div>
+                                                                        </a>
+                                                                    </div>
+                                                                ))
+                                                        ) : (
+                                                            <div>
+                                                                <h5>Данный пользователь не принимал участия в
+                                                                    опросах</h5>
+                                                            </div>
+                                                        )}
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    )
-                                    : (
-                                        <div>
-                                            <h5>Данный пользователь скрыл свою историю голосов</h5>
-                                        </div>
-                                    )}
+                                        )
+                                        : (
+                                            <div>
+                                                <h5>Данный пользователь скрыл свою историю голосов</h5>
+                                            </div>
+                                        )}
+                                </div>
                             </div>
-                        </div>
-                    )
-                    : (
-                        <h5 style={{justifyContent: 'center', display: 'flex'}}> Кажется, {user.username}, скрыл свой профиль.</h5>
+                        )
+                        : (
+                            <h5 style={{justifyContent: 'center', display: 'flex'}}> Кажется, {user.username}, скрыл
+                                свой профиль.</h5>
                         )}
                 </div>
             </div>
